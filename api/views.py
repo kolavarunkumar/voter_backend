63fc6da4b6b59f73f3ed12aa24d4d19b3a59700d
KOLA VARUN KUMAR <133854142+kolavarunkumar@users.noreply.github.com>
2026-01-22 15:19:10 +0530

    Update views.py

--- api/views.py
+++ api/views.py
@@ -27,10 +27,11 @@ def search_voter(request):
         serializer = VoterSerializer(voters, many=True)
         return Response(serializer.data)
 
-    @api_view(['GET'])
-def test_db(request):
-    return Response({"count": Voter.objects.count()})
-
     except Exception as e:
         logger.error("SEARCH API FAILED", exc_info=True)
         return Response({"error": str(e)}, status=500)
+
+@api_view(['GET'])
+def test_db(request):
+    return Response({"count": Voter.objects.count()})
+
