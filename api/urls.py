a5764b284ec06b0a751a8c3cc29fb46674e32e36
KOLA VARUN KUMAR <133854142+kolavarunkumar@users.noreply.github.com>
2026-01-22 14:50:28 +0530

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import search_voter
+from .views import search_voter, test_db
 
 urlpatterns = [
     path('search/', search_voter),
+    path('test-db/', test_db),  # <-- temporary test
 ]
