f287eab288321536c9472f5d37eb19e5ff6e7bb8
KOLA VARUN KUMAR <133854142+kolavarunkumar@users.noreply.github.com>
2026-01-22 13:15:45 +0530

    Update models.py
    added def str(self)

--- api/models.py
+++ api/models.py
@@ -14,3 +14,6 @@ class Voter(models.Model):
     gender = models.CharField(max_length=5)
     door_no = models.CharField(max_length=50)
     epic_no = models.CharField(max_length=20)
+    
+def __str__(self):
+        return self.name
