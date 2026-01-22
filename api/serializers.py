cd2f60cbd5b2c078cbf1f7b84f33e4cc97d78aed
KOLA VARUN KUMAR <133854142+kolavarunkumar@users.noreply.github.com>
2026-01-22 13:51:18 +0530

    Update serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -2,18 +2,18 @@ from rest_framework import serializers
 from .models import Voter
 
 class VoterSerializer(serializers.ModelSerializer):
+    ward_no = serializers.IntegerField(required=False, allow_null=True)
+    ac_no = serializers.IntegerField(required=False, allow_null=True)
+    ps_no = serializers.IntegerField(required=False, allow_null=True)
+    sl_no = serializers.IntegerField(required=False, allow_null=True)
+    name = serializers.CharField(required=False, allow_blank=True, allow_null=True)
+    relation_name = serializers.CharField(required=False, allow_blank=True, allow_null=True)
+    relation = serializers.CharField(required=False, allow_blank=True, allow_null=True)
+    age = serializers.IntegerField(required=False, allow_null=True)
+    gender = serializers.CharField(required=False, allow_blank=True, allow_null=True)
+    door_no = serializers.CharField(required=False, allow_blank=True, allow_null=True)
+    epic_no = serializers.CharField(required=False, allow_blank=True, allow_null=True)
+
     class Meta:
         model = Voter
-        fields = [
-            'ward_no',
-            'ac_no',
-            'ps_no',
-            'sl_no',
-            'name',
-            'relation_name',
-            'relation',
-            'age',
-            'gender',
-            'door_no',
-            'epic_no',
-        ]
+        fields = '__all__'
