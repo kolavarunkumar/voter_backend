1435338e52d5a65269c526a7fc056d08c2cfa7a9
KOLA VARUN KUMAR <133854142+kolavarunkumar@users.noreply.github.com>
2026-01-22 15:01:56 +0530

    Update models.py

--- api/models.py
+++ api/models.py
@@ -1,5 +1,3 @@
-
-# Create your models here.
 from django.db import models
 
 class Voter(models.Model):
@@ -14,6 +12,6 @@ class Voter(models.Model):
     gender = models.CharField(max_length=5)
     door_no = models.CharField(max_length=50)
     epic_no = models.CharField(max_length=20)
-    
-def __str__(self):
+
+    def __str__(self):
         return self.name
